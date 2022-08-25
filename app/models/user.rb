0bf26e98381731fae8749fbdcb2120c33d91f410
Aryan Pal <aryanpalongraph@gmail.com>
2022-08-25 12:41:01 +0530

    Configure Admin Approval

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
+
+  enum role: {"user"=>0, "vendor"=>1, "admin"=>2}
 end
