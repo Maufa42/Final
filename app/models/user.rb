eb48e43e5f5d0aee34cf08a672a7e4fc0b8163db
Aryan Pal <aryanpalongraph@gmail.com>
2022-08-25 18:11:17 +0530

    Added Google Omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,22 @@ class User < ApplicationRecord
   has_many :vendors
   devise :database_authenticatable,:omniauthable, :registerable,
          :recoverable, :rememberable, :validatable
-         has_one_attached :Profile_Picture
+         has_one_attached :image
    
   enum role: {"user"=>0, "vendor"=>1, "admin"=>2}
+
+  def self.from_omniauth(auth)
+    
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0, 20]
+      user.fname = auth.info.name   # assuming the user model has a name
+      # user.image =s auth.info.image # assuming the user model has an image
+      # If you are using confirmable and the provider(s) you use validate emails, 
+      # uncomment the line below to skip the confirmation emails.
+      # user.skip_confirmation!
+    end
+  end
+
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_25_095435) do
+ActiveRecord::Schema.define(version: 2022_08_25_112939) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -80,6 +80,8 @@ ActiveRecord::Schema.define(version: 2022_08_25_095435) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "type"
+    t.string "provider"
+    t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
