1d4364f36da008bba73c20181df38167c8cfc4b0
Aryan Pal <aryanpalongraph@gmail.com>
2022-08-25 13:12:02 +0530

    🚀 Added Active Storage

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
+         has_one_attached :Profile_Picture
+         has_one_attached :doc
   enum role: {"user"=>0, "vendor"=>1, "admin"=>2}
 end
